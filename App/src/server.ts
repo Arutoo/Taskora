65feabf403127decc8bd5cf540a00ee02bbfe30e
DlegendZ <raynaldarvanlim@gmail.com>
2026-05-04 16:47:00 +0700

    update server

--- App/src/server.ts
+++ App/src/server.ts
@@ -1,8 +1,26 @@
 import 'dotenv/config';
+import http from 'http';
+import { Server } from 'socket.io';
 import app from './app';
+import { initSockets } from './sockets';
+import { setSocketServer } from './utils/socketEmitter';
 
 const PORT = parseInt(process.env.PORT ?? '3000', 10);
 
-app.listen(PORT, () => {
+const httpServer = http.createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: process.env.CLIENT_ORIGIN ?? '*',
+    credentials: true,
+  },
+});
+
+setSocketServer(io);
+initSockets(io);
+
+httpServer.listen(PORT, () => {
   console.log(`Taskora backend running on port ${PORT}`);
 });
+
+export default httpServer;
